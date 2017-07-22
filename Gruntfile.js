d242dd660154821af49895faf86341b42de2d6ad
Agacia <agacia@gmail.com>
2017-07-22 10:16:58 +0200

    Modification of Gruntfile.js, addiing CSS and SASS folders and files

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,27 +2,28 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    sass: {
-      options: {
-        sourceMap: true
-      },
-      dist: {
-        files: {
-          'css/main.css': 'sass/main.sass'
-        }
-      }
-    }
-  });
-    imagemin: {
-    dynamic: {
-        files: [{
-            expand: true,
-            cwd: 'images/',
-            src: ['**/*.{png,jpg,gif}'],
-            dest: 'images/build/'
-        }]
-    }
-}
+  	sass: {
+  		options: {
+  			sourceMap: true
+  		},
+  		dist: {
+  			files: {
+  				'main.css': 'main.sass'
+  			}
+  		}
+  	},
+
+  	imagemin: {
+  		dynamic: {
+  			files: [{
+  				expand: true,
+  				cwd: 'images/',
+  				src: ['**/*.{png,jpg,gif}'],
+  				dest: 'images/build/'
+  			}]
+  		}
+  	},
+
     watch: {
     scripts: {
         files: ['sass/*.sass'],
@@ -32,22 +33,14 @@ module.exports = function(grunt) {
         },
     }
 }
-    browserSync: {
-    bsFiles: {
-        src : 'assets/css/*.css'
-    },
-    options: {
-        server: {
-            baseDir: "./"
-        }
-    }
-}
 
+  });
   // Load the plugins tasks
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-browser-sync');
+
   // Default task(s).
-  grunt.registerTask('default', ['sass', 'imagemin', 'watch', 'browserSync']);
+
+  grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
 };
